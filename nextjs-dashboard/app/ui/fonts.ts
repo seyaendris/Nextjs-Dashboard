64e6fbecdace9acd4cacd09dd6360fb0ff42d69f
seyaendris <seide9894@gmail.com>
2024-07-16 20:51:27 +0300

    optimizing images and fonts

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ["latin"]
+});
+
+export const lusitana = Lusitana({
+    subsets: ["latin"],
+    weight: ['400', '700'],
+});
+
