9da9acb52eefd72e88badde5cb4305e527f32cf5
seyaendris <seide9894@gmail.com>
2024-07-19 22:27:13 +0300

    nextjs-dashboard/app/ui/invoices/pagination.tsx

--- nextjs-dashboard/app/ui/invoices/pagination.tsx
+++ nextjs-dashboard/app/ui/invoices/pagination.tsx
@@ -3,7 +3,6 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 
